68e8b28a2fd442471e08882c09057b5013837b5d
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-02-07 17:45:00 +0530

    More property display

--- qtjs/oboy_headers/basic.js
+++ qtjs/oboy_headers/basic.js
@@ -1,6 +1,7 @@
 var server = createServer(function(req, res) {
-    print("Connection!");
+    print("Connection! " + req.remoteAddress + ":" + req.remotePort);
     print("User agent " + req.headers['user-agent']);
+    print(req.method, req.httpVersion, req.url, req.path);
 
     var data = "Must it be assumed that because we are engineers, beauty is not our concern? -- Gustave Eiffel";
     res.writeHead(200, {
